9d2125971b964dbbdc5c9d25c5c8c6b8426308ba
hiroto.ohira <hiroto100114@gmail.com>
2022-03-27 18:32:25 +0900

    Add qiit app.

---
+++ qiita/handler/user_handler.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"pancake.maker/api/gen/api"
+)
+
+type userHandler struct{}
+
+func NewUserHandle() *userHandler {
+	return &userHandler{}
+}
+
+func (h *userHandler) Get(ctx context.Context, r *api.UserRequest) (*api.UserResponse, error) {
+	if r.Id == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "ユーザーの ID を指定してください")
+	}
+
+	res := &api.UserResponse{
+		User: &api.User{
+			Id:   24,
+			Name: "John",
+		},
+	}
+
+	return res, nil
+}
---
+++ qiita/server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"pancake.maker/api/gen/api"
+	"pancake.maker/handler"
+)
+
+const port = 50051
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	api.RegisterUserGetServiceServer(server, handler.NewUserHandle())
+	// To debug on grpc_cli.
+	reflection.Register(server)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		server.Serve(lis)
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	server.GracefulStop()
+}
